51976d3fed9e422c6d9f878d6f512db18799266a
Sherius <Sheriusek@wp.pl>
2019-09-08 01:32:34 +0200

    first commit

---
+++ src/main/java/kalkulator/demo/Kalkulator.java
@@ -0,0 +1,4 @@
+package kalkulator.demo;
+
+public class Kalkulator {
+}
---
+++ src/test/java/kalkulator/demo/KalkulatorApplicationTests.java
@@ -0,0 +1,16 @@
+package kalkulator.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KalkulatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
