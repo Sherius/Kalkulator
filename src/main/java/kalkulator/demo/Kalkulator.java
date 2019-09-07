a17c3e526a05fb84f426f3f53e10a427243914c8
Sherius <Sheriusek@wp.pl>
2019-09-08 01:41:59 +0200

    first commit

--- src/main/java/kalkulator/demo/Kalkulator.java
+++ src/main/java/kalkulator/demo/Kalkulator.java
@@ -1,4 +1,17 @@
 package kalkulator.demo;
 
 public class Kalkulator {
+    int a = 20;
+    int b = 30;
+    public int addAtoB(int a, int b)
+        {
+            return = a + b;
+        }
+    public int SubstractAFromB(int a, int b)
+    {
+        return = a-b;
+
+    }
+
+
 }
